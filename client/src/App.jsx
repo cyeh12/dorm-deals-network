a9bfdaf62a8675a6bcc43bc117938b55592e2059
chunchenyeh <cyeh12@hawk.illinoistech.edu>
2025-07-05 12:56:40 -0700

    Show HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import AppNavbar from './components/AppNavbar';
-// import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage';
 // import LoginPage from './pages/LoginPage';
 // import RegisterPage from './pages/RegisterPage';
 // import MarketplacePage from './pages/MarketplacePage';
@@ -20,7 +20,7 @@ function App() {
         <AppNavbar />
         <div className="content-wrap">
           <Routes>
-            {/* <Route path="/" element={<HomePage />} /> */}
+            <Route path="/" element={<HomePage />} />
             {/* <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/marketplace" element={<MarketplacePage />} />
