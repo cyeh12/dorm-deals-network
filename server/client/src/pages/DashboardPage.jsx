127ab808edc0cb4227f16fa85abeee877f98858a
chunchenyeh <cyeh12@hawk.illinoistech.edu>
2025-07-06 16:59:05 -0700

    Wording change in Dashboard Page

--- server/client/src/pages/DashboardPage.jsx
+++ server/client/src/pages/DashboardPage.jsx
@@ -179,7 +179,7 @@ const DashboardPage = () => {
         <Col md={6} className="mb-4">
           <Card>
             <Card.Header className="d-flex justify-content-between align-items-center">
-              <h5 className="mb-0">Recent from {user.university}</h5>
+              <h5 className="mb-0">Recent Marketplace Activity</h5>
               <Button as={Link} to="/browse" variant="outline-primary" size="sm">
                 Browse All
               </Button>
