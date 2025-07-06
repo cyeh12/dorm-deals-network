fba077cf276ec0237fd5870c09061917e658307a
chunchenyeh <cyeh12@hawk.illinoistech.edu>
2025-07-06 09:44:21 -0700

    add the / route (debugging)

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Catch-all route for React Router (client-side routing)
-app.get(/(.*)/, (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
