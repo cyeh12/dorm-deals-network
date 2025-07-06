60df6fbf977aaf020953ba5ada78e0a1d00a1bd5
chunchenyeh <cyeh12@hawk.illinoistech.edu>
2025-07-05 21:04:43 -0700

    Update Favicon

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Barebones route to serve a simple HTML page
 app.get('/', (req, res) => {
