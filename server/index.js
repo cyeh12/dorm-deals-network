79cd7740f812ac7c8f8d0ddb3ebb53186fcb012d
chunchenyeh <cyeh12@hawk.illinoistech.edu>
2025-07-05 21:10:08 -0700

    remove typo

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Add middlware
 app.use(cors());
-app.use(bodyParser.json());e
+app.use(bodyParser.json());
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/dist')));
