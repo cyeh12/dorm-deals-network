d1e157cd50f5a45a52d16c133b4bd394ab4aa64c
chunchenyeh <cyeh12@hawk.illinoistech.edu>
2025-07-05 20:28:51 -0700

    fix bug

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ app.post('/api/register', async (req, res) => {
   }
 });
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
